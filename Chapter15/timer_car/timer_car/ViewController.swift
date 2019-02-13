131b4c8c69bb9e5b07493e9b4e62a47d84310bdb
Hiroki Ikeuchi <hikeuchi@icloud.com>
2019-02-13 20:29:23 +0900

    initial commit

---
+++ Chapter15/timer_car/timer_car/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timer_car
+//
+//  Created by Hiroki Ikeuchi on 2019/02/13.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
