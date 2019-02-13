b654d537919a97a7241cfc6b0dbaa836d3c3acf5
Hiroki Ikeuchi <hikeuchi@icloud.com>
2019-02-13 20:40:08 +0900

    Chapter15/P484

--- Chapter15/timer_car/timer_car/ViewController.swift
+++ Chapter15/timer_car/timer_car/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var car: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // make a timer
+        Timer.scheduledTimer(timeInterval: 0.1,
+                             target      : self,
+                             selector    : #selector(self.step),
+                             userInfo    : nil,
+                             repeats     : true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func step() {
+        // move vertical
+        car.center.x += 10
+        // if out of right side...
+        let carWidth = car.bounds.width
+        if car.center.x>(view.bounds.width + carWidth/2) {
+            // return left sise
+            car.center.x = -carWidth
+            // set random y coordinate
+            let viewH = view.bounds.height  // viewH = view Height
+            car.center.y = CGFloat(arc4random_uniform(UInt32(viewH)))
+        }
+        
+    }
 
 }
 
