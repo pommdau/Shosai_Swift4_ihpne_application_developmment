4aaf39dfb458c875880b4ac16f25cdaa7b90a724
Hiroki Ikeuchi <hikeuchi@icloud.com>
2019-02-15 23:54:57 +0900

    Chapter17-1-A/P526

--- Section17/Section17-1-A/Section17-1-A/ViewController.swift
+++ Section17/Section17-1-A/Section17-1-A/ViewController.swift
@@ -36,17 +36,49 @@ class ViewController: UIViewController {
         return image!
     }
     
+    // 角丸四角形のパスを作る
+    func makeBoundRectImage(width w:CGFloat, height h:CGFloat, corner r:CGFloat) -> UIImage {
+        // イメージ処理の開始
+        let size = CGSize(width: w, height: h)
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        let context = UIGraphicsGetCurrentContext()
+        
+        // 角丸四角形の描画
+        let drawRect = CGRect(x: 0, y: 0, width: w, height: h)
+        let drawPath = UIBezierPath(roundedRect: drawRect, cornerRadius: r)
+        // パスを塗る
+        context?.setFillColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0)
+        drawPath.fill()
+        // パスを書く
+        context?.setStrokeColor(red: 1.0, green: 0, blue: 0, alpha: 1.0)
+        drawPath.stroke()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image!
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // 四角形のイメージを作る
-        let boxImage = makeBoxImage(width: 240, height: 180)
+//        // 四角形のイメージを作る
+//        let boxImage = makeBoxImage(width: 240, height: 180)
+//        // イメージビューに設定する
+//        let boxView = UIImageView(image: boxImage)
+//        // 画面に表示する
+//        boxView.center = view.center
+//        view.addSubview(boxView)
+        
+        // 角丸四角形のイメージを作る
+        let roundedBoxImage = makeBoundRectImage(width: 240, height: 180, corner: 20)
         // イメージビューに設定する
-        let boxView = UIImageView(image: boxImage)
+        let roundedBoxView = UIImageView(image: roundedBoxImage)
         // 画面に表示する
-        boxView.center = view.center
-        view.addSubview(boxView)
+        roundedBoxView.center = view.center
+        view.addSubview(roundedBoxView)
         
     }
 
